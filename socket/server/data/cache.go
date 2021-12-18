ee0279c60f7f9a811f18a80a42672797fb60df82
Paulo <git@pauloo27.dev>
2021-12-17 21:46:18 -0300

    feat(socket): add simple cache checksum

--- socket/server/data/cache.go
+++ socket/server/data/cache.go
@@ -1,9 +1,34 @@
 package data
 
+import (
+	"crypto/md5"
+	"sort"
+	"strings"
+)
+
+func calcCurrentInboxMD5(inboxID int) [16]byte {
+	var msgsIDs []string
+	for _, msg := range unreadMessages[inboxID].Messages {
+		msgsIDs = append(msgsIDs, msg.ID)
+	}
+	sort.Strings(msgsIDs)
+	x := []byte(strings.Join(msgsIDs, " "))
+	return md5.Sum(x)
+}
+
 func refreshCache(inboxID int) {
+	prevSum := calcCurrentInboxMD5(inboxID)
 	for {
+		curSum := calcCurrentInboxMD5(inboxID)
 		<-cacheTimers[inboxID].C
 		ClearInboxCache(inboxID)
-		fetchUnreadMessage(inboxID)
+		err := fetchUnreadMessage(inboxID)
+		if err != nil {
+			panic(err)
+		}
+		if curSum != prevSum {
+			// TODO: notify listeners
+		}
+		prevSum = curSum
 	}
 }
